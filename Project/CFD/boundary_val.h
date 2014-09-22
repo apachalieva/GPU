6efa34d01691d51104c5cfba829bfedd9715b445
Praktikant Acc074 <p074@atradig139.informatik.tu-muenchen.de>
2014-09-22 20:42:18 +0200

    Const BC for CFD

--- Project/CFD/boundary_val.h
+++ Project/CFD/boundary_val.h
@@ -4,13 +4,22 @@
 /**
  * The boundary values of the problem are set.
  */
-void boundaryvalues( int    imax,
-		     int    jmax,
-		     double **U,
-		     double **V,
-		     float  *imgU,
-		     float  *imgV,
-		     int    **Flag
-		   );
+void findBoundaryvalues( int    imax,
+			  int    jmax,
+			  double **U,
+			  double **V,
+			  float  *imgU,
+			  float  *imgV,
+			  int    **Flag
+			);
+
+void setBoundaryvalues( int    imax,
+			 int    jmax,
+			 double **U,
+			 double **V,
+			 double **boundU, 
+			 double **boundV,
+			 int    **Flag
+			);
 
 #endif
